a7d48a84b2133e8f8faedde328bf3954c4989721
Pannous <info@pannous.net>
2018-01-19 15:15:23 +0100

    jekyll build

---
+++ _site/cat.md
@@ -0,0 +1,4 @@
+[𓄂](𓄂)  
+
+𓏇𓇋𓅱𓃠 Miw Miau Minkat  
+𓏇𓇋𓇋𓏏𓄛 Mietze Katze ￩ MiceCatch  
---
+++ _site/fix-wiki.sh
@@ -0,0 +1,10 @@
+git fetch --all
+git pull
+for f in `ls *.md`; do
+ sed -i 's/\s*$/  /g' $f
+ sed -i 's/<>/⇔/g' $f
+ sed -i 's/^  $//' $f
+ sed -i 's/\[\[\(.*?\)\]\]/[\1](\1)/g' $f
+done
+git commit -a --allow-empty-message -m '⇔'
+git push
