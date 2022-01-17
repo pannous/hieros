874c475ddae9084fe4f28ee74ce805f23f9125f3
Pannous <info@pannous.net>
2022-01-17 23:30:46 +0100

    sync docs

--- dicts/swadesh/gypt.js
+++ dicts/swadesh/gypt.js
@@ -1,37 +1,26 @@
 #!/usr/bin/env node
-require('/me/dev/js/extensions.js')()
+require('/Users/me/dev/js/extensions.js')()
 last={}
-for(line of read(
-	if(!line.English)continue 
-	word=line.English.replace(/ .*/,"").trim()
-	// console.log(word)
-	// continue 
+for(line of read_csv("swadesh.tsv")){
+	nr=int(line[0])
+	word=line[1]
+	if(nr<91 || nr>146)continue 
+	if(!word)continue 
+	word=word.replace(/^to /,"").trim()
+	word=word.replace(/ .*/,"").trim()
 	for(eg of read_csv("../../my_egyptian_dictionary.csv")){
-		trans=eg[2]
 		glyph=eg[0]
+		trans=eg[1]
+		comment=eg[2]
 		if(!trans||!glyph)continue 
-		// if(len(last[word])>=glyph.length)continue 
-			// console.log(trans)
-		// if(str(trans).contains(word))
-		// if(trans.contains(word))
-		// 	console.log("~~~~~",word,eg.join("\t"))
-		if(trans.match(new RegExp("\\b"+word+"\\b"))){
+		ok = trans.match(new RegExp("\\b"+word+"\\b"))
+		// ok = ok || comment && comment.match(new RegExp("\\b"+word+"\\b"))
+		if(ok){
 			console.log(line[0]+"\t"+word+"\t"+eg.join("\t"))
 			last[word]=glyph	
 		}
 
 	}
 	if(!last[word])
-		for(eg of read_csv("../../my_egyptian_dictionary.csv")){
-		trans=eg[2]
-		if(!trans)continue 
-		if(trans.contains(word)){
-			console.log(line[0]+"\t"+word+"\t"+eg.join("\t"))
-			break
-		}
-	}
-	if(!last[word])
 			console.log(line[0]+"\t"+word)
-
-
 }
